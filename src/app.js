91ae84900e4ff488d21a4b9b3d7649bf84e5e3b2
harshitadaswani <87665916+harshitadaswani@users.noreply.github.com>
2021-08-04 02:33:22 +0530

    js added

--- src/app.js
+++ src/app.js
@@ -1,1 +1,30 @@
-//https://api.funtranslations.com/translate/minion.json
+import "./styles.css";
+
+console.log("script is working");
+
+var translateButton = document.querySelector("#translate-button");
+var translateInput = document.querySelector("#translate-input");
+var translateOutput = document.querySelector("#translate-output");
+var url = "https://api.funtranslations.com/translate/minion.json";
+
+translateButton.addEventListener("click", buttonClickHandler);
+
+function getTranslatedURL(text) {
+  var a = `${url}?text=${text}`;
+  return a;
+}
+
+function buttonClickHandler(event) {
+  console.log("button clicked");
+  var input = translateInput.value;
+  var finalURL = getTranslatedURL(input);
+  console.log(finalURL);
+  fetch(finalURL)
+    .then((response) => response.json())
+    .then((json) => {
+      translateOutput.innerText = json.contents.translated;
+    })
+    .catch((error) => {
+      alert("OOPS! An Error occured. Please try again in a few minutes");
+    });
+}
